ee9c4b7a42cfcdd19b41d21029be46435e1353d9
Declan McGuinness <60525697+TheFiftGuy@users.noreply.github.com>
2023-04-24 15:05:15 +0100

    Created empty maps/classes to start putting in GGPO stuff

---
+++ Source/ServerRollback/Public/GGPOGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GGPOGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SERVERROLLBACK_API AGGPOGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ServerRollback/Public/GGPOGameStateBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "GGPOGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SERVERROLLBACK_API AGGPOGameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
